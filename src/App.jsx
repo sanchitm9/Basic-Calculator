41ff7060f5f9f21c2ea73b704df773f7fbe0ed80
Sanchit <mishrasanchit9302@gmail.com>
2024-02-22 23:36:58 +0530

    Added evaluation functionality and all clear function

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ function App() {
   const [calVal, setCalVal] = useState("");
   const onButtonClick = (buttonText) => {
     if (buttonText === "C") {
-
+      setCalVal("");
     } else if (buttonText === "=") {
-      
+      const result = eval(calVal);
+      setCalVal(result);
     } else {
       const newDisplayValue = calVal + buttonText;
       setCalVal(newDisplayValue);
